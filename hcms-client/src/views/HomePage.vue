3467745764386c5687f85d02aee25b1acfbf9e3d
Edgar Dimas Isaadrazak <edgar.dimas.ir@gmail.com>
2022-04-19 07:22:28 +0700

    initial commit

---
+++ hcms-client/src/views/HomePage.vue
@@ -0,0 +1,74 @@
+<template>
+  <!-- Start Services -->
+  <div id="services" class="services-box">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="title-box">
+            <h2>Services</h2>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="owl-carousel owl-theme">
+            <div class="item">
+              <div class="serviceBox">
+                <div class="service-icon">
+                  <i class="fa fa-h-square" aria-hidden="true"></i>
+                </div>
+                <h3 class="title">Lorem ipsum dolor</h3>
+                <p class="description">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Accusantium consequuntur.
+                </p>
+                <a href="#" class="new-btn-d br-2">Read More</a>
+              </div>
+            </div>
+            <div class="item">
+              <div class="serviceBox">
+                <div class="service-icon">
+                  <i class="fa fa-heart" aria-hidden="true"></i>
+                </div>
+                <h3 class="title">Lorem ipsum dolor</h3>
+                <p class="description">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Accusantium consequuntur.
+                </p>
+                <a href="#" class="new-btn-d br-2">Read More</a>
+              </div>
+            </div>
+            <div class="item">
+              <div class="serviceBox">
+                <div class="service-icon">
+                  <i class="fa fa-hospital-o" aria-hidden="true"></i>
+                </div>
+                <h3 class="title">Lorem ipsum dolor</h3>
+                <p class="description">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Accusantium consequuntur.
+                </p>
+                <a href="#" class="new-btn-d br-2">Read More</a>
+              </div>
+            </div>
+            <div class="item">
+              <div class="serviceBox">
+                <div class="service-icon">
+                  <i class="fa fa-stethoscope" aria-hidden="true"></i>
+                </div>
+                <h3 class="title">Lorem ipsum dolor</h3>
+                <p class="description">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Accusantium consequuntur.
+                </p>
+                <a href="#" class="new-btn-d br-2">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Services -->
+</template>
