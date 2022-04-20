a1353f5787f9275741d509d0ef64e0417e06f0c9
ratih sanjaya <ratihsanjayaw@gmail.com>
2022-04-20 15:52:55 +0700

    adding feature login

---
+++ dietive/src/stores/food.js
@@ -0,0 +1,73 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import dummy from '../../dummy.json'
+
+export const useFoodStore = defineStore({
+  id: "food",
+  state: () => ({
+    allFoodData: [],
+    province: [],
+    city: [],
+    isLogin: false
+  }),
+
+  actions: {
+    async searchFeature(keyword) {
+      try {
+        //  const response = await axios.get("https://api.spoonacular.com/recipes/complexSearch", {
+        //    params: {
+        //     apiKey: "87331fcef85c482ebf4e26e8cceaefc1",
+        //     minCalories: 0,
+        //     number: 6,
+        //     minFat: 0,
+        //     sort: "calories",
+        //     query: keyword
+        //    }
+        //  })
+
+        const data = dummy;
+
+        this.allFoodData = data.results;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async getAllProvince() {
+      try {
+        const response = await axios.get(
+          "https://dev.farizdotid.com/api/daerahindonesia/provinsi"
+        );
+        this.province = response.data.provinsi;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async getAllCities(id) {
+      try {
+        const response = await axios.get(
+          `https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi=${id}`
+        );
+        this.city = response.data.kota_kabupaten;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async registerAction(payload) {
+      try {
+        const response = await axios.post("http://localhost:3000/register", payload);
+        console.log(response);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async loginAction(payload) {
+      try {
+        const response = await axios.post("http://localhost:3000/login", payload);
+        // localStorage.setItem("access_token", )
+        console.log(response);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+});
