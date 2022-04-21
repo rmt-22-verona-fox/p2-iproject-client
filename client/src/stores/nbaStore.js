a27145693c1bafbb4b02a09f297fbc320d342955
Oktavicto <oktavicto@gmail.com>
2022-04-21 11:18:46 +0700

    preparing to deploy

--- client/src/stores/nbaStore.js
+++ client/src/stores/nbaStore.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "ur-basketball-site.herokuapp.com";
 export const useNbaStore = defineStore({
   id: "nba",
   state: () => ({}),
--- client/src/stores/trainingStore.js
+++ client/src/stores/trainingStore.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "ur-basketball-site.herokuapp.com";
 export const useTrainingStore = defineStore({
   id: "training",
   state: () => ({}),
